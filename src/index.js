d6d92d97dbe9d9046b86f1cff43f3b1e07a23c75
Andrey Pleskunou <apleskunov@bk.ru>
2020-10-21 02:59:51 +0300

    fix: invalid fix :((

--- src/index.js
+++ src/index.js
@@ -1,46 +1,31 @@
-const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
-};
-
 function decode(expr) {
-    // write your solution here
+    console.log(expr);
+    const l = expr.split(/(.){10}/g, ',');
+    console.log(l);
+    expr = expr.replace(/(11){1}/g, '-');
+    console.log(expr);
+    expr = expr.replace(/(10){1}/g, '.');
+    console.log(expr);
+    expr = expr.replace(/[*]{10}/g, ' ');
+    console.log(expr);
+    expr = expr.replace(/-----/g, '0').replace('----.', '9').replace(/(\-\-\-\.\.){1}/g, '8').replace(/(\-\-\.\.\.){1}/g, '7');
+    expr = expr.replace(/(\-\.\.\.\.){1}/g, '6').replace('.....', '5').replace(/(\.\.\.\.\-){1}/g, '4').replace(/(\.\.\.\-\-){1}/g, '3');
+    expr = expr.replace('..---', '2').replace('.----', '1').replace('--..', 'z').replace(/(\-\.\-\-){1}/g, 'y');
+    expr = expr.replace('-..-', 'x').replace(/(\.\.\.\-){1}/g, 'v').replace('--.-', 'q').replace('.--.', 'p');
+    expr = expr.replace(/\.\-\.\./g, 'l').replace('.---', 'j').replace(/(\.\.\.\.){1}/g, 'h').replace(/(\.\.\-\.){1}/g, 'f');
+    expr = expr.replace(/(\-\.\-\.){1}/g, 'c').replace('-...', 'b');
+    expr = expr.replace(/(\.\-\-){1}/g, 'w').replace(/(\.\.\-)/g, 'u').replace(/(\.\.\.){1}/g, 's').replace(/(\.\-\.)/g, 'r');
+    expr = expr.replace(/(\-){3}/g, 'o').replace('-.-', 'k').replace(/(\-\.\.){1}/g, 'd').replace(/(\-\-\.){1}/g, 'g');
+    expr = expr.replace(/(\-\.){1}/g, 'n').replace(/(\-\-){1}/g, 'm').replace(/(\.\.){1}/g, 'i').replace(/(\.\-)/g, 'a');
+    expr = expr.replace(/(\.){1}/g, 'e').replace(/(\-){1}/g, 't') /* .replace(/0/g, '') */ ;
+    console.log(expr);
+    return expr;
 }
 
 module.exports = {
     decode
+
 }
+const expr = "00000000100000111010101010111100111011100000001011111110101011111010101010101010";
+const result = "ed3ca775";
+decode(expr);
