c7cd8a66dddb7e10a611f4d7f0b718d08ea53837
Andrey Pleskunou <apleskunov@bk.ru>
2020-10-21 03:08:10 +0300

    fix : fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 function decode(expr) {
-    console.log(expr);
-    const l = expr.split(/(.){10}/g, ',');
-    console.log(l);
-    expr = expr.replace(/(11){1}/g, '-');
-    console.log(expr);
-    expr = expr.replace(/(10){1}/g, '.');
-    console.log(expr);
-    expr = expr.replace(/[*]{10}/g, ' ');
-    console.log(expr);
+    expr = expr.replace(/(11){1}/g, '-').replace(/(10){1}/g, '.').replace(/[*]{10}/g, ' ');
     expr = expr.replace(/-----/g, '0').replace('----.', '9').replace(/(\-\-\-\.\.){1}/g, '8').replace(/(\-\-\.\.\.){1}/g, '7');
     expr = expr.replace(/(\-\.\.\.\.){1}/g, '6').replace('.....', '5').replace(/(\.\.\.\.\-){1}/g, '4').replace(/(\.\.\.\-\-){1}/g, '3');
     expr = expr.replace('..---', '2').replace('.----', '1').replace('--..', 'z').replace(/(\-\.\-\-){1}/g, 'y');
@@ -17,7 +9,7 @@ function decode(expr) {
     expr = expr.replace(/(\.\-\-){1}/g, 'w').replace(/(\.\.\-)/g, 'u').replace(/(\.\.\.){1}/g, 's').replace(/(\.\-\.)/g, 'r');
     expr = expr.replace(/(\-){3}/g, 'o').replace('-.-', 'k').replace(/(\-\.\.){1}/g, 'd').replace(/(\-\-\.){1}/g, 'g');
     expr = expr.replace(/(\-\.){1}/g, 'n').replace(/(\-\-){1}/g, 'm').replace(/(\.\.){1}/g, 'i').replace(/(\.\-)/g, 'a');
-    expr = expr.replace(/(\.){1}/g, 'e').replace(/(\-){1}/g, 't') /* .replace(/0/g, '') */ ;
+    expr = expr.replace(/(\.){1}/g, 'e').replace(/(\-){1}/g, 't').replace(/0/g, '');
     console.log(expr);
     return expr;
 }
